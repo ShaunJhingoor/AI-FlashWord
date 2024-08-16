df02bc168be5ddb9c1b39ab99dc8dc96f07921cc
John Doe <patrickrgt@gmail.com>
2024-08-15 23:01:06 -0400

    generate styling

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -340,188 +340,141 @@ const DecksPage = () => {
           generate flashcards
         </h1>
 
-        <div className="h-[50vh] relative bg-gradient-to-b from-[#111111] to-[#323232] shadowStroke dropShadow w-[60vh] flex flex-col justify-start items-center gap-[4vh] px-[4vh] py-[4vh] rounded-[2vh]">
-          <div className="flex flex-col justify-start w-full gap-[1vh]">
-            <input
-              type="text"
-              label="Deck Name"
-              aria-label="Deck Name"
-              onChange={(e) => setDeckName(e.target.value)}
-              placeholder="Enter Deck Title"
-              className="w-full h-[5vh] text-[5vh] text-white font-bold bg-transparent border-none outline-none placeholder-[#989898]"
-              required
-            />
-            <h3 className="text-[2vh] text-[#989898] font-medium capitalize">
-              use ai to generate a deck
-            </h3>
+        {/* Generation container */}
+        <div className="w-full flex items-center justify-around">
+          {/* Deck Generation */}
+          <div className="flex flex-col justify-center items-center gap-[2vh]">
+            <div className="h-[50vh] relative bg-gradient-to-b from-[#111111] to-[#323232] shadowStroke dropShadow w-[60vh] flex flex-col justify-start items-center gap-[4vh] px-[4vh] py-[4vh] rounded-[2vh]">
+              <div className="flex flex-col justify-start w-full gap-[1vh]">
+                <input
+                  type="text"
+                  label="Deck Name"
+                  aria-label="Deck Name"
+                  onChange={(e) => setDeckName(e.target.value)}
+                  placeholder="Enter Deck Title"
+                  className="w-full h-[5vh] text-[5vh] text-white font-bold bg-transparent border-none outline-none placeholder-[#989898]"
+                  required
+                />
+                <h3 className="text-[2vh] text-[#989898] font-medium capitalize">
+                  use ai to generate a deck
+                </h3>
+              </div>
+
+              <div className="w-full h-full">
+                <textarea
+                  id="deckContent"
+                  value={deckContent}
+                  placeholder="build your deck"
+                  onChange={(e) => setDeckContent(e.target.value)}
+                  required
+                  className="w-full h-full rounded-[2vh] pl-[1.5vh] border-[#FFFFFF] border-[0.15vh] p-[2vh] text-[2vh] bg-[#1B1B1B] text-[#EBEBEB] font-semibold resize-none"
+                ></textarea>
+              </div>
+            </div>
+            <div className="flex inset-0 justify-center items-center w-[60vh]">
+              <div className="flex-1 w-full">
+                <h2 className="text-[3vh] text-[#989898]">how many cards?</h2>
+              </div>
+              <input
+                type="text"
+                className="lowercase transition-all ease-in-out text-black placeholder:text-[#383838] underline text-[2vh] font-semibold text-center placeholder:text-[2vh] w-[10%] p-[2vh]"
+                placeholder="1"
+                maxLength="10"
+              />
+              <button
+                onClick={(e) => handleSubmit(e)}
+                className="lowercase bg-[#383838] hover:bg-[#929292] transition-all ease-in-out text-[#FFFFFF] text-[2vh] font-semibold rounded-full px-[4vh] p-[2vh] box-border"
+              >
+                generate
+              </button>
+            </div>
           </div>
 
-          <div className="w-full h-full">
-            <textarea
-              id="deckContent"
-              value={deckContent}
-              placeholder="build your deck"
-              onChange={(e) => setDeckContent(e.target.value)}
-              required
-              className="w-full h-full rounded-[2vh] pl-[1.5vh] border-[#FFFFFF] border-[0.15vh] p-[2vh] text-[2vh] bg-[#1B1B1B] text-[#EBEBEB] font-semibold resize-none"
-            ></textarea>
-
-            {/* <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              onClick={(e) => handleSubmit(e)}
-              sx={{ mt: 2, borderRadius: 2, boxShadow: 2 }}
-            >
-              Add Deck
-            </Button> */}
+          {/* PDF Generation */}
+          <div className="flex flex-col justify-center items-center gap-[2vh]">
+            <div className="h-[50vh] relative bg-gradient-to-b from-[#D9D9D9] to-[#E3E3E3] shadowStroke dropShadow w-[60vh] flex flex-col justify-start items-center gap-[4vh] px-[4vh] py-[4vh] rounded-[2vh]">
+              <div className="flex flex-col justify-start w-full gap-[1vh]">
+                <input
+                  type="text"
+                  label="Deck Name"
+                  aria-label="Deck Name"
+                  onChange={(e) => setDeckName(e.target.value)}
+                  placeholder="Enter Deck Title"
+                  className="w-full h-[5vh] text-[5vh] text-white font-bold bg-transparent border-none outline-none placeholder-[#989898]"
+                  required
+                />
+                <h3 className="text-[2vh] text-[#989898] font-medium capitalize">
+                  upload PDF to generate deck
+                </h3>
+                <input
+                  type="file"
+                  accept=".pdf"
+                  id="fileInput"
+                  onChange={(e) => handleFileChange(e)}
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                />
+                <label
+                  htmlFor="fileInput"
+                  className="flex flex-col underline text-[4vh] text-[#989898] font-semibold justify-center items-center w-full h-[30vh] bg-[#D9D9D9] border-[#141414] border-dashed border-[0.25vh] rounded-[2vh] text-center cursor-pointer"
+                >
+                  <img src="/cloud.png"></img>
+                  Browse file to upload
+                </label>
+              </div>
+            </div>
+            <div className="flex lowercase justify-around items-center gap-[2vh] w-[60vh]">
+              <div className="flex-1 w-full">
+                <h2 className="text-[3vh] text-[#989898]">how many cards?</h2>
+              </div>
+              <input
+                type="text"
+                className="lowercase transition-all ease-in-out text-black placeholder:text-[#383838] underline text-[2vh] font-semibold text-center placeholder:text-[2vh] w-[10%] p-[2vh]"
+                placeholder="1"
+                maxLength="10"
+              />
+              <button
+                onClick={(e) => handleFileSubmit(e)}
+                className="lowercase bg-[#383838] hover:bg-[#929292] transition-all ease-in-out text-[#FFFFFF] text-[2vh] font-semibold rounded-full px-[4vh] p-[2vh] box-border"
+              >
+                generate
+              </button>
+            </div>
           </div>
-        </div>
-
-        <div className="flex lowercase justify-around items-center gap-[2vh] w-[60vh]">
-          <div className="flex-1 w-full">
-            <h2 className="text-[3vh] text-[#989898]">how many cards?</h2>
-          </div>
-          <input
-            type="text"
-            className="lowercase transition-all ease-in-out text-black placeholder:text-[#383838] underline text-[2vh] font-semibold text-center placeholder:text-[2vh] w-[10%] p-[2vh]"
-            placeholder="1"
-            maxLength="10"
-          />
-          <button
-            onClick={(e) => handleSubmit(e)}
-            className="lowercase bg-[#383838] hover:bg-[#929292] transition-all ease-in-out text-[#FFFFFF] text-[2vh] font-semibold rounded-full px-[4vh] p-[2vh] box-border"
-          >
-            generate
-          </button>
         </div>
       </div>
 
-      <Container
-        maxWidth="md"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          background: "linear-gradient(to right, #f472b6, #a855f7, #3b82f6)",
-          minHeight: "100vh",
-          minWidth: "100vw",
-          padding: "20px",
-        }}
-      >
-   
-
-        <Box
-          mb={4}
-          p={3}
-          borderRadius={2}
-          boxShadow={3}
-          bgcolor="background.paper"
-          sx={{
-            maxWidth: "600px",
-            background: "linear-gradient(to right, #ffffff, #f9f9f9)",
-          }}
-        >
-          <Typography variant="h6" gutterBottom>
-            Upload PDF to Generate Deck
-          </Typography>
-          <input
-            type="file"
-            accept=".pdf"
-            onChange={(e) => handleFileChange(e)}
-            ref={fileInputRef}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={(e) => handleFileSubmit(e)}
-            sx={{ mt: 2 }}
+      <div className="grid grid-cols-3 w-[80%] m-auto gap-[5vh] pt-[4vh]">
+        {decks.map((deck) => (
+          <div
+          onClick={() => handleDeckClick(deck)}
+            key={deck.id}
+            className="cursor-pointer flex flex-col gap-[2vh] shadowStroke justify-center items-center bg-gradient-to-b from-[#111111] to-[#323232] h-[40vh] rounded-[4vh]"
           >
-            Generate Deck from PDF
-          </Button>
-        </Box>
+            <h1
+              className="text-[5vh] text-white font-semibold cursor-pointer"
+            >
+              {deck.id}
+            </h1>
+            <div className="flex gap-[2vh] px-[5vh]">
+            <button onClick={(e) => {
+                e.stopPropagation(); 
+                handleEdit(deck);
+            }}>
+                <img src="/edit.png"></img>
+              </button>
+              <button onClick={(e) => {
+                e.stopPropagation(); 
+                handleDelete(deck.id);
+            }}>
+                <img src="/delete.png"></img>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
 
-        {/* Listing Decks */}
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            minWidth: "100%",
-          }}
-        >
-          <Grid container spacing={1} justifyContent="center">
-            {decks.map((deck) => (
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                key={deck.id}
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                <Box
-                  sx={{
-                    border: "1px solid #ccc", // Lighter border for a cleaner look
-                    borderRadius: 0.5, // Slightly rounded corners
-                    background: "#fafafa", // Slightly off-white background for a paper effect
-                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)", // Subtle shadow to mimic paper lift
-                    textAlign: "center",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "30rem",
-                    height: "15rem",
-                    transition: "box-shadow 0.3s, transform 0.3s", // Smooth transition for hover effects
-                    marginTop: "2rem",
-                    marginBottom: "2rem",
-                    "&:hover": {
-                      boxShadow: "0 4px 12px rgba(0, 0, 0, 0.2)", // Slightly stronger shadow on hover
-                      transform: "scale(1.01)", // Slightly enlarge the box on hover
-                    },
-                  }}
-                >
-                  <Typography
-                    variant="subtitle1"
-                    component="div"
-                    fontWeight="bold"
-                    onClick={() => handleDeckClick(deck)}
-                    fontSize="2rem"
-                    sx={{
-                      cursor: "pointer",
-                      textDecoration: "underline",
-                      color: "#1976d2",
-                      mb: 1,
-                      transition: "color 0.3s", // Smooth transition for hover effect
-                      "&:hover": {
-                        color: "#1565c0", // Darker color on hover
-                      },
-                    }}
-                  >
-                    {deck.id}
-                  </Typography>
-                  <Box display="flex" justifyContent="center" mb={1}>
-                    <IconButton
-                      onClick={() => handleEdit(deck)}
-                      sx={{ color: "#1976d2", mr: 1, fontSize: "2rem" }}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      onClick={() => handleDelete(deck.id)}
-                      sx={{ color: "#d32f2f" }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Box>
-                </Box>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-
+    
         {/* Edit Deck Modal */}
         <Dialog
           open={isEditing}
@@ -701,7 +654,6 @@ const DecksPage = () => {
             </IconButton>
           </DialogActions>
         </Dialog>
-      </Container>
     </>
   );
 };
